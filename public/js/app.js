ec5325b85d9e3fee065f24be6087e0ab8414fd26
makevoid <makevoid@gmail.com>
2017-07-23 12:59:35 +0100

    update basic insurance flow

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,41 @@
-console.log("hello world")
+// this script uses zepto.js
+
+// https://github.com/bertani/insurETH/blob/master/frontend/js/flyether.js
+
+var dom = $(window)
+
+var rates = {
+  gbp_btc:  150
+}
+
+var get_rates = function() {
+  $.getJSON("https://bitpay.com/api/rates/gbp", function(data){
+    rates.gbp_btc = data.rate
+    dom.trigger("rates_updated")
+  })
+  $.getJSON("https://shapeshift.io/marketinfo/btc_eth", function(data){
+    rates.btc_eth = data.rate
+    dom.trigger("rates_updated")
+  })
+}
+
+var update_insured_totals = function(evt){
+  var value = evt.target.value || 50
+  var value_btc = value/rates.gbp_btc
+  $(".insure_amount_gbp").html(value)
+  $(".insure_amount_btc").html(value_btc.toFixed(2))
+  $(".insure_amount_eth").html((value_btc*rates.btc_eth).toFixed(2))
+}
+
+$(function(){
+
+  var btc_gbp = get_rates()
+
+  $("input[name=insure_amount]").on("mousemove", update_insured_totals)
+
+  dom.on("rates_updated", update_insured_totals)
+
+  // $(".button.insure"      ).on("click", do_insure)
+  // $(".button.deposit-done").on("click", deposit_triggered)
+
+})
