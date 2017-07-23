9d59f44a211be748592719fbe9a97cc7d9d2133f
makevoid <makevoid@gmail.com>
2017-07-23 13:30:36 +0100

    naive compiler via shelling out to solc

--- lib/compiler.rb
+++ lib/compiler.rb
@@ -1,7 +1,18 @@
+require_relative '../env'
+
 module Compiler
 
-  def call
+  # naive compilation shelling out to solc
 
+  def call
+    name = "Poolsureth"
+
+    # hardcoded libraries addresses for simplicity
+    oraclize_address     = "0xe4b113092efc4367731f7c775a5dd3db6adbb3a5"
+    solidity_stringutils = "0xe4b113092efc4367731f7c775a5dd3db6adbb3a5"
+
+
+    puts `cd #{PATH}/contracts && solc #{name}.sol | solc --link --libraries usingOraclize:#{oraclize_address},Solidity_stringutils:#{solidity_stringutils}`
   end
 
   module_function :call
@@ -9,7 +20,9 @@ module Compiler
   private
 
   module ModuleMethods
-    
+
   end
 
 end
+
+Compiler.call
